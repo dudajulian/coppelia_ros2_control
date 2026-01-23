b25d48b06a975b7732918154564314d5010e9b95
jvgruber <julian.v.gruber@gmail.com>
2026-01-23 14:45:59 +0000

    update files

--- launch/create_2_control.launch.py
+++ launch/create_2_control.launch.py
@@ -1,4 +1,5 @@
 # Copyright 2020 ros2_control Development Team
+# Modified by Julian Duda 2025
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,6 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, RegisterEventHandler
@@ -168,8 +170,8 @@ def generate_launch_description():
         control_node,
         robot_state_pub_node,
         robot_controller_spawner,
-        delay_rviz_after_joint_state_broadcaster_spawner,
         delay_joint_state_broadcaster_after_robot_controller_spawner,
+        # delay_rviz_after_joint_state_broadcaster_spawner,
     ]
 
     return LaunchDescription(declared_arguments + nodes)
